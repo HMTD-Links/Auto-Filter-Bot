01824aa58707f1fdbedacef1442f421a4860c198
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2025-01-21 10:41:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import time
 import asyncio
 import uvloop
 
-# Pyrogram imports
+# pyrogram imports
 from pyrogram import types
 from pyrogram import Client
 from pyrogram.errors import FloodWait
@@ -12,13 +12,9 @@ from pyrogram.errors import FloodWait
 from aiohttp import web
 from typing import Union, Optional, AsyncGenerator
 
-# telethon imports 
-from telethon import TelegramClient
-from telethon.sessions import StringSession
-
 # local imports
 from web import web_app
-from info import LOG_CHANNEL, API_ID, API_HASH, BOT_TOKEN, USER_STRING_SESSION, PORT, BIN_CHANNEL, ADMINS, DATABASE_URL
+from info import LOG_CHANNEL, API_ID, API_HASH, BOT_TOKEN, PORT, BIN_CHANNEL, ADMINS, DATABASE_URL
 from utils import temp, get_readable_time
 
 # pymongo and database imports
@@ -27,10 +23,12 @@ from database.ia_filterdb import Media
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
+uvloop.install()
+
 class Bot(Client):
     def __init__(self):
         super().__init__(
-            name='Auto-Filter-Bot',
+            name='Auto_Filter_Bot',
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
@@ -38,20 +36,18 @@ class Bot(Client):
         )
 
     async def start(self):
+        temp.START_TIME = time.time()
         b_users, b_chats = await db.get_banned()
         temp.BANNED_USERS = b_users
         temp.BANNED_CHATS = b_chats
         client = MongoClient(DATABASE_URL, server_api=ServerApi('1'))
-        
         try:
             client.admin.command('ping')
             print("Pinged your deployment. You successfully connected to MongoDB!")
         except Exception as e:
             print("Something Went Wrong While Connecting To Database!", e)
             exit()
-
         await super().start()
-
         if os.path.exists('restart.txt'):
             with open("restart.txt") as file:
                 chat_id, msg_id = map(int, file)
@@ -60,7 +56,6 @@ class Bot(Client):
             except:
                 pass
             os.remove('restart.txt')
-        
         temp.BOT = self
         await Media.ensure_indexes()
         me = await self.get_me()
@@ -69,43 +64,71 @@ class Bot(Client):
         temp.B_NAME = me.first_name
         username = '@' + me.username
         print(f"{me.first_name} is started now 🤗")
-        
+        #groups = await db.get_all_chats_count()
+        #for grp in groups:
+            #await save_group_settings(grp['id'], 'fsub', "")
         app = web.AppRunner(web_app)
         await app.setup()
         await web.TCPSite(app, "0.0.0.0", PORT).start()
-        
         try:
             await self.send_message(chat_id=LOG_CHANNEL, text=f"<b>{me.mention} Restarted! 🤖</b>")
         except:
             print("Error - Make sure bot admin in LOG_CHANNEL, exiting now")
             exit()
-        
         try:
             m = await self.send_message(chat_id=BIN_CHANNEL, text="Test")
             await m.delete()
         except:
             print("Error - Make sure bot admin in BIN_CHANNEL, exiting now")
             exit()
-        
         for admin in ADMINS:
             await self.send_message(chat_id=admin, text="<b>✅ Bot Restarted Successfully..</b>")
 
-async def main():
-    user_client = TelegramClient(StringSession(USER_STRING_SESSION), API_ID, API_HASH)
-    await user_client.start()
+    async def stop(self, *args):
+        await super().stop()
+        print("Bot Stopped! Bye...")
 
-    bot = Bot()
-    try:
-        await bot.start()
-        print("Bot Started Successfully")
-    except FloodWait as vp:
-        wait_time = get_readable_time(vp.value)
-        print(f"Flood Wait Occurred, Sleeping For {wait_time}")
-        await asyncio.sleep(vp.value)
-        print("Now Ready For Deploying!")
-        await bot.start()
+    async def iter_messages(self: Client, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:
+        """Iterate through a chat sequentially.
+        This convenience method does the same as repeatedly calling :meth:`~pyrogram.Client.get_messages` in a loop, thus saving
+        you from the hassle of setting up boilerplate code. It is useful for getting the whole chat messages with a
+        single call.
+        Parameters:
+            chat_id (``int`` | ``str``):
+                Unique identifier (int) or username (str) of the target chat.
+                For your personal cloud (Saved Messages) you can simply use "me" or "self".
+                For a contact that exists in your Telegram address book you can use his phone number (str).
+                
+            limit (``int``):
+                Identifier of the last message to be returned.
+                
+            offset (``int``, *optional*):
+                Identifier of the first message to be returned.
+                Defaults to 0.
+        Returns:
+            ``Generator``: A generator yielding :obj:`~pyrogram.types.Message` objects.
+        Example:
+            .. code-block:: python
+                async for message in app.iter_messages("pyrogram", 1000, 100):
+                    print(message.text)
+        """
+        current = offset
+        while True:
+            new_diff = min(200, limit - current)
+            if new_diff <= 0:
+                return
+            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
+            for message in messages:
+                yield message
+                current += 1
+
+app = Bot()
+try:
+    app.run()
+except FloodWait as vp:
+    time = get_readable_time(vp.value)
+    print(f"Flood Wait Occured, Sleeping For {time}")
+    asyncio.sleep(vp.value)
+    print("Now Ready For Deploying !")
+    app.run()
     
-    await bot.idle()  # This keeps the bot running until it's stopped
-
-if __name__ == "__main__":
-    asyncio.run(main())  # Replacing the deprecated loop with asyncio.run()
