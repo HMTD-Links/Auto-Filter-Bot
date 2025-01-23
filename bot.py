ed51f2757bddec2aa66c3c06e8d2b1a4e5a19429
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2025-01-23 16:35:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ uvloop.install()
 class Bot(Client):
     def __init__(self):
         super().__init__(
-            name='Filter-Bot',
+            name='ASC-Filter-Bot',
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
